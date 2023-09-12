ae9690b1953e6401d4a8ffb5d75f1c51ebace289
user <user@ITWILL->
2023-09-12 19:34:59 +0900

    상품 수정 오류 해결 / 내가 쓴 글(판매) 추가

--- grgrmain/src/main/java/com/grgr/dto/MyBoardWriteDTO.java
+++ grgrmain/src/main/java/com/grgr/dto/MyBoardWriteDTO.java
@@ -8,11 +8,11 @@ import lombok.Data;
 public class MyBoardWriteDTO {
 	private int uno;
 	private int bno;
-    private String title;
-    private String content;
-    private int viewCnt;
-    private String keyword;
-    private Date regdate;
-    private String nickName;
-    private String boardtype;
+	private String title;
+	private String content;
+	private int viewCnt;
+	private String keyword;
+	private Date regdate;
+	private String nickName;
+	private String boardtype;
 }
